b7e3697efe56cc0ebebf6bb1edc5ea062fef3fb3
Nicole <neanwebdev@gmail.com>
2023-04-06 15:22:37 -0700

    Updates schedule item interface and data

--- src/data/default-schedule.js
+++ src/data/default-schedule.js
@@ -3,78 +3,104 @@ export const defaultSchedule = [
     stepNumber: 1,
     name: 'Levain',
     type: 'Rest',
-    duration: '00:01'
+    duration: '00:01',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 2,
     name: 'Mix',
     type: 'Action',
-    duration: ''
+    duration: '',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 3,
     name: 'Rest',
     type: 'Rest',
-    duration: '0:01'
+    duration: '0:01',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 4,
     name: 'Salt',
     type: 'Action',
-    duration: ''
+    duration: '',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 5,
     name: 'Rest',
     type: 'Rest',
-    duration: '0:30'
+    duration: '0:30',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 6,
     name: 'Stretch & Fold',
     type: 'Action',
-    duration: ''
+    duration: '',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 7,
     name: 'Rest',
     type: 'Rest',
-    duration: '0:30'
+    duration: '0:30',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 8,
     name: 'Stretch & Fold',
     type: 'Action',
-    duration: ''
+    duration: '',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 9,
     name: 'Rest',
     type: 'Rest',
-    duration: '0:30'
+    duration: '0:30',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 10,
     name: 'Pre-Shape',
     type: 'Action',
-    duration: ''
+    duration: '',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 11,
     name: 'Rest',
     type: 'Rest',
-    duration: '0:30'
+    duration: '0:30',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 12,
     name: 'Shape',
     type: 'Action',
-    duration: ''
+    duration: '',
+    actualDuration: '',
+    completed: false,
   },
   {
     stepNumber: 13,
     name: 'Bulk Fermentation',
     type: 'Rest',
-    duration: '4:00'
+    duration: '4:00',
+    actualDuration: '',
+    completed: false,
   }
 ]
