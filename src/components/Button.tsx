d23d3c336414fab636bd455862f538954afb38e8
Nicole <neanwebdev@gmail.com>
2023-05-01 23:15:53 -0700

    Adds more properties to Button for more customization

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,15 @@
 interface ButtonProps {
   ariaLabel: string;
+  isFullWidth?: boolean;
+  classes?: string;
   children: React.ReactNode;
   onClick: () => void;
 }
 
-export default function Button({ ariaLabel, children, onClick }: ButtonProps) {
+export default function Button({ ariaLabel, isFullWidth, classes, children, onClick }: ButtonProps) {
   return(
-    <button aria-label={ariaLabel} onClick={onClick} className="flex justify-center w-full rounded bg-slate-200 hover:bg-slate-400 py-2 px-4">{ children }</button>
+    <button aria-label={ariaLabel} onClick={onClick} className={`flex justify-center rounded bg-slate-200 hover:bg-slate-400 py-2 px-4 ${ isFullWidth && 'w-full' } ${ classes }`}>
+      { children }
+    </button>
   )
 }
