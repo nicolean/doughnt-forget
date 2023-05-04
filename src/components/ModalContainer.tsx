452c1ddfa75e30b22f98eff7e22d8cee863012f7
Nicole <neanwebdev@gmail.com>
2023-05-04 12:38:37 -0700

    Improves mobile responsiveness for modal container

--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -19,8 +19,8 @@ export default function ModalContainer({ isOpen, setIsOpen, title, children, sho
 
   return (
     isOpen ?
-      <div className="fixed top-0 left-0 w-screen h-screen sm:p-10 bg-gray-900/50 z-10">
-        <div ref={wrapperRef} className="absolute flex flex-col sm:block sm:left-2/4 sm:-translate-x-2/4 rounded shadow bg-white w-screen sm:w-9/12 lg:w-6/12 xl:w-4/12 h-screen sm:max-h-full sm:h-auto">
+      <div className="fixed flex sm:items-center justify-center top-0 left-0 w-screen h-screen sm:p-10 bg-gray-900/50 z-10">
+        <div ref={wrapperRef} className="flex flex-col rounded shadow bg-white w-screen sm:w-9/12 lg:w-6/12 xl:w-4/12 h-[100dvh] sm:max-h-full sm:h-auto">
           <div className="flex justify-between p-6">
             <h2 className="text-xl">{ title }</h2>
             <button aria-label="Close modal" onClick={() => setIsOpen(false)}><X /></button>
